c5fc6cf64c3176d8738d644e7113ceda898d6583
mendes <jrmmendes@outlook.com>
2025-04-12 13:53:46 -0300

    chore: remove unused file

--- cli.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render, Text } from 'ink';
-import { SamlessBanner } from './samless-banner';
-const Demo = () => /*#__PURE__*/React.createElement(Text, null, " ", /*#__PURE__*/React.createElement(SamlessBanner, null), " ");
-render(/*#__PURE__*/React.createElement(Demo, null));
